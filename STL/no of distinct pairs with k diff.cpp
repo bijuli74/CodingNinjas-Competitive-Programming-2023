318392f1826fceaffcaa81aff5e8e562218c5a04
Soumyadip Bijuli <46708112+bijuli74@users.noreply.github.com>
2023-05-24 00:18:36 +0530

    Update no of distinct pairs with k diff.cpp

--- STL/no of distinct pairs with k diff.cpp
+++ STL/no of distinct pairs with k diff.cpp
@@ -15,6 +15,67 @@ int kDiffPairs(vector < int > & arr, int n, int k) {
     return s.size();
 }
 
+/*
+	Time complexity: O(N) 
+	Space complexity: O(N)
+	
+	Where 'N' is the size of the array.
+*/
+
+#include<unordered_map>
+
+int kDiffPairs(vector < int > & arr, int n, int k) {
+
+    // Initialise a hashmap to store frequencies.
+    unordered_map < int, int > mp;
+
+    // Initialise a variable to store the count.
+    int count = 0;
+
+    // Start traversing through the vector.
+    for (int i = 0; i < n; i++) {
+
+        // If difference is not equal to zero.
+        if (k != 0) {
+            if (mp.find(arr[i]) != mp.end()) {
+
+                // Number present in map.
+                continue;
+            }
+            if (mp.find(k + arr[i]) != mp.end()) {
+
+                // Number with difference equal to k found.
+                count++;
+            }
+            if (mp.find(arr[i] - k) != mp.end()) {
+
+                // Number with difference equal to k found.
+                count++;
+            }
+
+            // Increase frequency.
+            mp[arr[i]]++;
+        }
+
+        // If k is equal to zero.
+        else {
+
+            // Increase frequency.
+            mp[arr[i]]++;
+
+            // If frequency equal to two.
+            if (mp[arr[i]] == 2) {
+
+                // Pair with difference equal to zero found.
+                count++;
+            }
+        }
+    }
+
+    // Return count.
+    return count;
+}
+
 // #include <bits/stdc++.h> 
 // int kDiffPairs(vector < int > & arr, int n, int k) {
     
