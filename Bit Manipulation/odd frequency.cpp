a13def8ff002b3fabd71f382db351c8f2c915c0b
Soumyadip Bijuli <46708112+bijuli74@users.noreply.github.com>
2023-05-28 00:17:19 +0530

    Create odd frequency.cpp

---
+++ Bit Manipulation/odd frequency.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t; cin>>t;
+    while(t--){
+        int n; cin>>n;
+        int ans=0;
+        for(int i=0; i<n; ++i){
+            int a; cin>>a;
+            ans ^=a;
+        }
+        cout<<ans<<'\n';
+    }
+    return 0;
+}
