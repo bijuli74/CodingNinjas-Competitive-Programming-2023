011c39a4e69d1d6828ad2cbe2aedfb714c7d25c5
Soumyadip Bijuli <46708112+bijuli74@users.noreply.github.com>
2023-05-28 01:46:39 +0530

    Create min count.cpp

---
+++ Dynamic Programming/min count.cpp
@@ -0,0 +1,10 @@
+int minCount(int n)
+{
+	vector<int> dp(n+1, n);
+	dp[0]=0;
+	dp[1]=1;
+	for(int i=2; i<=n; ++i)
+		for(int j=1; j*j<=i; ++j)
+			dp[i] = min(dp[i], dp[i-j*j]+1);
+	return dp[n];
+}
