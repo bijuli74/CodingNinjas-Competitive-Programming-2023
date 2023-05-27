6dce3a8413995f91e5ed8ba4e0dd6cb805239e18
Soumyadip Bijuli <46708112+bijuli74@users.noreply.github.com>
2023-05-28 00:38:48 +0530

    Create find prime nos.cpp

---
+++ Number Theory/find prime nos.cpp
@@ -0,0 +1,17 @@
+vector<int> primeNumbersTillN(int N) 
+{ //Linear sieve method
+ 	vector<int> lp(N+1), pr;
+	 for(int i=2; i<=N; ++i){
+		 if(lp[i]==0){
+			 lp[i]=i;
+			 pr.push_back(i);
+		 }
+		 for(int j=0; i*pr[j]<=N; ++j){
+			 lp[i*pr[j]] = pr[j];
+			 if(pr[j]==lp[i])
+			 	break;
+		 }
+	 }
+
+	 return pr;
+}
